709b451710ab10076f88457ad57463a007c1c61e
Mathias G <ghbm@aarhus.dk>
2023-11-10 15:01:26 +0100

    Test

--- main.bat
+++ main.bat
@@ -4,6 +4,4 @@ python -m venv .venv
 call .venv\Scripts\activate
 pip install "C:\Repos\OpenOrchestrator"
 
-echo %2
-
 python process.py %1 %2 %3 %4
--- process.py
+++ process.py
@@ -2,6 +2,11 @@
 
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 
+import sys
+
+for a in sys.argv:
+    print(a)
+
 orchestrator_connection = OrchestratorConnection.create_connection_from_args()
 
 orchestrator_connection.log_info("Hello World!")
