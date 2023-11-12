bdf87e607e16b4c2fac844ee042ab9c33d2dc6c3
Mathias G <ghbm@aarhus.dk>
2023-11-12 12:10:27 +0100

    Test

--- process.py
+++ process.py
@@ -1,15 +1,10 @@
 """A hello world script for OpenOrchestrator robots."""
 
-# from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
+from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 
-import sys
+orchestrator_connection = OrchestratorConnection.create_connection_from_args()
 
-for a in sys.argv:
-    print(a)
+orchestrator_connection.log_info("Hello World!")
 
-# orchestrator_connection = OrchestratorConnection.create_connection_from_args()
-
-# orchestrator_connection.log_info("Hello World!")
-
-# args = orchestrator_connection.process_arguments
-# orchestrator_connection.log_info(args)
+args = orchestrator_connection.process_arguments
+orchestrator_connection.log_info(args)
